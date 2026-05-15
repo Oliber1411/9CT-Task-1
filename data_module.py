ba03154fe10ad716fc46d8145c0fafc56b57a5ad
Oliber1411 <Oliver.vanraalte@education.nsw.gov.au>
2026-05-15 12:30:17 +1000

    Ermm aded stuff

---
+++ data_module.py
@@ -0,0 +1,6 @@
+import os, time
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
---
+++ main.py
@@ -0,0 +1,20 @@
+import os, time
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+def menu():
+    print("--Data--Viewer--Interface--")
+    print("|                          |")
+    print("|    1. View datasheet     |")
+    print("|                          |")
+    print("|    2. View datasheet     |")
+    print("|                          |")
+    print("|    3. View datasheet     |")
+    print("|                          |")
+    print("|    4. View datasheet     |")
+    print("----------------------------")
+
+if __name__ == "__main__":
+    menu()
